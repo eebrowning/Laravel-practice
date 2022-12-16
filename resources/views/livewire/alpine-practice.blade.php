ca2134b58c44f2e7dfa9d0e7ae8e2e4a25294016
eebrowning <eebrowning828@gmail.com>
2022-12-16 15:41:39 -0700

    alpine/livewire practice

--- resources/views/livewire/alpine-practice.blade.php
+++ resources/views/livewire/alpine-practice.blade.php
@@ -7,8 +7,8 @@
         <button @click="open = true">Show More...</button>
 
         <ul x-show="open" @click.outside="open = false">
-            <li><button wire:click="archive">Archive</button></li>
-            <li><button wire:click="delete">Delete</button></li>
+            <li>thing1</li>
+            <li>thing2</li>
         </ul>
     </div>
 
