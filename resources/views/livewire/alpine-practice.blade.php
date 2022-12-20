01a687b84945647759b2e25d1854a78ae8a2e48f
eebrowning <eebrowning828@gmail.com>
2022-12-20 11:25:21 -0700

    planning to allow main image on product page to change based on the image selected in the gallery below

--- resources/views/livewire/alpine-practice.blade.php
+++ resources/views/livewire/alpine-practice.blade.php
@@ -1,6 +1,6 @@
 
 <div id=image-block>
-
+<!-- I need to have an onClick on each gallery image that will replace the default image with the clicked image -->
         <img id='image-main' src='https://cdn.shopify.com/s/files/1/1065/4262/products/{{$mediaCollection[0]['name']}}?v={{$mediaCollection[0]['shopifyId']}}' alt=""/>
 
         <div id='image-options'>
