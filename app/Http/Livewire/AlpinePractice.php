84ecd31fcf57a10c25b37b46d3044eb82d2b7751
eebrowning <eebrowning828@gmail.com>
2022-12-20 12:06:34 -0700

    clickable gallery achieved, but dependent on javascript

--- app/Http/Livewire/AlpinePractice.php
+++ app/Http/Livewire/AlpinePractice.php
@@ -6,12 +6,20 @@ use Livewire\Component;
 
 class AlpinePractice extends Component
 {
-
+    
     public $mediaCollection;
- 
+    
+    public $defaultImg;
+    public $defaultId;
+    
+    public function displayImg(){
+       $defaultImg=$mediaCollection[0]['name'];
+        $defaultId=$mediaCollection[0]['shopifyId'];
+    }
 
     public function render()
     {
+        
         return view('livewire.alpine-practice');
     }
 }
--- resources/views/livewire/alpine-practice.blade.php
+++ resources/views/livewire/alpine-practice.blade.php
@@ -1,13 +1,30 @@
 
 <div id=image-block>
 <!-- I need to have an onClick on each gallery image that will replace the default image with the clicked image -->
+        <script>
+                let image;
+                let shopify;
+                function main(info){
+                        info= info.split(" ");
+                        let image= info[0];
+                        let shopify= info[1];
+                        let main= document.getElementById('image-main');
+
+                        main.src= `https://cdn.shopify.com/s/files/1/1065/4262/products/${image}?v=${shopify}`
+                }
+        </script>
+        
         <img id='image-main' src='https://cdn.shopify.com/s/files/1/1065/4262/products/{{$mediaCollection[0]['name']}}?v={{$mediaCollection[0]['shopifyId']}}' alt=""/>
+        <!-- <img id='image-main' src='https://cdn.shopify.com/s/files/1/1065/4262/products/{{$mediaCollection[0]['name']}}?v={{$mediaCollection[0]['shopifyId']}}' alt=""/> -->
+
+
 
         <div id='image-options'>
         @foreach($mediaCollection as $image)
-        <img src='https://cdn.shopify.com/s/files/1/1065/4262/products/{{$image['name']}}?v={{$image['shopifyId']}}' alt=""/>
-        @endforeach
-        
+        <img class="{{$image['name']}} {{$image['shopifyId']}}" src='https://cdn.shopify.com/s/files/1/1065/4262/products/{{$image['name']}}?v={{$image['shopifyId']}}' 
+                onclick="main('{{$image['name']}} {{$image['shopifyId']}}');" 
+                alt=""/>
+        @endforeach 
         </div>
 
 
